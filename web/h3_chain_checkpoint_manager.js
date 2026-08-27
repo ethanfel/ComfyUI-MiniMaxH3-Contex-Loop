a36a04ec873eda25541cbf224bcefc4e10bd67f8
Ethanfel <ethanfel@users.noreply.github.com>
2026-08-27 21:25:26 +0200

    Refine project asset action toolbar

--- web/h3_chain_checkpoint_manager.js
+++ web/h3_chain_checkpoint_manager.js
@@ -9,18 +9,18 @@ import {
     checkpointSelectionJson,
     formatCheckpointBytes,
     selectedCheckpointRevision,
-} from "./h3_checkpoint_manager_core.mjs?v=0.6.43";
+} from "./h3_checkpoint_manager_core.mjs?v=0.6.44";
 import {
     parsePlanJson,
     planToJson,
     promptValueToText,
-} from "./h3_chain_plan_core.mjs?v=0.6.43";
-import {applyCheckpointRevisionSet} from "./h3_chain_review_core.mjs?v=0.6.43";
-import * as promptCompanionSync from "./h3_prompt_companion_sync.mjs?v=0.6.43";
+} from "./h3_chain_plan_core.mjs?v=0.6.44";
+import {applyCheckpointRevisionSet} from "./h3_chain_review_core.mjs?v=0.6.44";
+import * as promptCompanionSync from "./h3_prompt_companion_sync.mjs?v=0.6.44";
 import {
     refreshRestoredPlanEditors,
     restoreConnectedPolicyInputs,
-} from "./h3_plan_restore_core.mjs?v=0.6.43";
+} from "./h3_plan_restore_core.mjs?v=0.6.44";
 
 const NODE_NAME = "MiniMaxH3ChainCheckpointManager";
 const PLAN_NAME = "MiniMaxH3ChainPlan";
